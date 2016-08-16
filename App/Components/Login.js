005478df4e83cf8e1acc604401fa8e25a7afcd92
siva <sivakumar@brillersys.com>
2016-08-16 15:55:44 +0530

    updated

--- App/Components/Login.js
+++ App/Components/Login.js
@@ -27,8 +27,8 @@ export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      uname: 'ramesh.palleri',
-      password: 'briller@inter1',
+      uname: '',
+      password: '',
       message: '',
       showProgress: false
     };    
--- App/settings.js
+++ App/settings.js
@@ -4,14 +4,14 @@
 
 //----------------------------------------
 // Change the following to switch between DEVEL and PRODUCTION
-var DEV = 0; //1 Development, 0 Production
+var DEV = 1; //1 Development, 0 Production
 //----------------------------------------
 
 var ServerName = (DEV) ? 'http://10.2.31.117/web-rp/' : 'https://informatics.intermolecular.com/'; //https://10.10.10.20/TAMS/index.cfm#/
 
 var serviceURL = ServerName + 'extjs/servicebus/Router.cfm'; 
-//var relayURL = ServerName + 'relay/toolPassDownRelay.cfc';
-var relayURL = ServerName + 'relay/relay.cfc';
+var relayURL = ServerName + 'relay/toolPassDownRelay.cfc';
+//var relayURL = ServerName + 'relay/relay.cfc';
 
 module.exports = {
 	serviceURL : serviceURL.toString(),
